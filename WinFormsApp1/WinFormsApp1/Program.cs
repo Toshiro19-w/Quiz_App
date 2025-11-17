e9ac9b6c4c111ffe6e076f2d9c3fdf7a7c31c504
plak101 <phamleanhkhoa2005@gmail.com>
2025-11-17 16:56:16 +0700

    sửa appsetting

--- WinFormsApp1/WinFormsApp1/Program.cs
+++ WinFormsApp1/WinFormsApp1/Program.cs
@@ -1,18 +1,29 @@
+using Microsoft.Extensions.Configuration;
 using WinFormsApp1.TestForm;
 
 namespace WinFormsApp1
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
+		/// <summary>
+		///  The main entry point for the application.
+		/// </summary>
+		/// 
+		public static IConfiguration Configuration;
+		[STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+			// To customize application configuration such as set high DPI settings or default font,
+			// see https://aka.ms/applicationconfiguration.
+
+			// Load appsettings.json cho WinForms
+			Configuration = new ConfigurationBuilder()
+				.SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile("appsettings.Development.json", optional: true)
+				.Build();
+
+			ApplicationConfiguration.Initialize();
             //Application.Run(new dangnhap());
             Application.Run(new dangnhap());
 		}
